50ed0daca6041adde687a92a135c18faf6657eda
mkukke <mkukke@BLRWX10247.internal.imsglobal.com>
2020-09-15 13:52:27 +0530

    Comment ADdted

--- src/test/java/org/tests/Tc1.java
+++ src/test/java/org/tests/Tc1.java
@@ -8,7 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Tc1 {
 	WebDriver driver;
-	
+	//comment added to this
 	@Test
 	public void tc1(){
 		WebDriverManager.chromedriver().setup();
